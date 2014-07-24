8b6885f047c49d032734b1f471ccf52ba80a4403
Davide Fiocco <davidefiocco@users.noreply.github.com>
2014-07-25 00:53:28 +0200

    Added LAMMPS scripts needed to deform KA configurations with specified parameters, with brief comments about usage.

---
+++ Code/KA model/Deform/deform.sh
@@ -0,0 +1,5 @@
+# bash script template to launch the athermal quasi-static deformation of the KA sample
+# requires a version of the LAMMPS executables to be placed in the current directory
+# the grep pipes are a dirty (but functional) to filter the long screen output of LAMMPS, and get the relevant part only (which is a tiny fraction of it)
+
+./lmp_linux < batch.in |grep -A5 minimization|grep -A3 PotEng|grep -B1 Loop|grep -v Loop|grep -v '\-\-' > results/deformation.log
