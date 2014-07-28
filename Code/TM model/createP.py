0061f6c2208d4785de1f002abc466846dc5a2691
Davide Fiocco <davide.fiocco@epfl.ch>
2014-07-28 23:41:09 +0200

    Added python scripts relative to the TM model, with brief comments about usage.

--- Code/TM model/createP.py
+++ Code/TM model/createP.py
@@ -7,30 +7,30 @@ p = float(sys.argv[2])
 gamma_max = int(sys.argv[3])
 np.random.seed(123456)
 
-allPplus = np.eye(M)
-allPminus = np.eye(M)
+allPfor = np.eye(M)
+allPback = np.eye(M)
 
 for gammastar in range(1, gamma_max):
     
-    Pplus = np.zeros((M, M))
-    Pminus = np.zeros((M, M))    
+    Pfor = np.zeros((M, M))
+    Pback = np.zeros((M, M))    
     unstable = np.random.rand(M) < p 
     for i in xrange(M):
         if unstable[i]:
             dest = np.random.randint(M) 
-            Pplus[dest,i] = 1
+            Pfor[dest,i] = 1
         else:
-            Pplus[i,i] = 1 
+            Pfor[i,i] = 1 
     for i in xrange(M):
         if unstable[i]: 
             dest = np.random.randint(M)
-            Pminus[dest,i] = 1
+            Pback[dest,i] = 1
         else:
-            Pminus[i,i] = 1
+            Pback[i,i] = 1
 
-    allPplus = np.dot(Pplus, allPplus) 
-    allPminus = np.dot(allPminus, Pminus)
+    allPfor = np.dot(Pfor, allPfor) 
+    allPback = np.dot(allPback, Pback)
     
-Pplus = np.dot(allPminus, allPplus) 
+Pplus = np.dot(allPback, allPfor) 
 plt.spy(Pplus)  
 plt.show()
